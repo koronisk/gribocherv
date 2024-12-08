be5053d8e9cafbd602e60cd7e0b0d3c90f4c8869
koronisk <106849559+koronisk@users.noreply.github.com>
2024-12-08 18:11:52 +0200

    entrypoint

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -22,4 +22,5 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 # from the container itself.
 printf "\033[1m\033[33mcontainer@gloffi~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
+cd data
 exec env ${PARSED}
