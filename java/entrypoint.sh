223e7b0480f4aba34933be0c1ec4d53e81c2a10a
koronisk <106849559+koronisk@users.noreply.github.com>
2024-12-16 05:30:36 +0200

    logo

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -9,7 +9,7 @@ export INTERNAL_IP
 # Switch to the container's working directory
 
 # Print Java version
-printf "\033[1m\033[33mcontainer@gloffi~ \033[0mjava -version\n"
+printf "\033[1m\033[33mgloffi@host~ \033[0mjava -version\n"
 java -version
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
@@ -19,7 +19,18 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@gloffi~ \033[0m%s\n" "$PARSED"
+
+printf "
+                     ________.__          _____  _____.__ 
+                    /  _____/|  |   _____/ ____\/ ____\__|
+                   /   \  ___|  |  /  _ \   ___\   __\|  |
+                   \    \_\  \  |_(  <_> )  |   |  |  |  |
+                    \______  /____/\____/|__|   |__|  |__|
+                           \/                             
+
+
+                   "
+printf "\033[1m\033[33mgloffi@host~ \033[0m%s\n" "$PARSED"
 
 # shellcheck disable=SC2086
 exec ${PARSED}
