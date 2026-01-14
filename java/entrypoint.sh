a0d8bd5657ca5644e780e710e4c0dab295a481bc
koronisk <koronisk.github@gmail.com>
2026-01-15 00:13:28 +0200

    mm3

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -1,21 +1,12 @@
-# Default the TZ environment variable to UTC.
+
 TZ=${TZ:-UTC}
 export TZ
 
-# Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Print Java version
-# uptime
-
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
 
 printf "
 
@@ -29,6 +20,4 @@ printf "
 "
 
 date
-# java -version
-# shellcheck disable=SC2086
 exec ${PARSED}
